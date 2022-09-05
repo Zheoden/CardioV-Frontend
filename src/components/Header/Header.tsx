0d54d81506612c256e8f29c0d5437427138d7f60
Zheoden <schifferjulian@gmail.com>
2022-09-05 12:04:59 -0300

    add new logos

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,105 +1,137 @@
-import { useState } from 'react';
-import Logo from '../../assets/Corazon.svg';
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Avatar from '@mui/material/Avatar';
-import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import Logout from '@mui/icons-material/Logout';
-import LogoutButton from '../Session/LogoutButton';
+import { Link } from 'react-router-dom';
+//import Logo from '../../assets/Logo-Oscuro.png';
+import Logo from '../../assets/Corazon.svg';
 
 import './Header.scss';
-import { Link } from 'react-router-dom';
+import { Logout } from '@mui/icons-material';
+import { Divider } from '@mui/material';
+import LogoutButton from '../Session/LogoutButton';
 
-interface HeaderProps {
-  open: boolean;
-  setOpen: (state: boolean) => void;
-}
+const pages = [
+  { displayName: 'Home', path: '/' },
+  { displayName: 'Profile', path: '/profile' },
+
+  
+  { displayName: 'Videos', path: '/videos' },
+];
 
 const Header = () => {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const openAvatar = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
   };
-  const handleClose = () => {
-    setAnchorEl(null);
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
   };
 
   return (
-    <div className={`header flex flex-row my-auto bg-sky-600 w-screen p-2 justify-between`}>
-      <div className='flex'>
+    <div className='header flex flex-row my-auto bg-sky-500 w-screen p-2 justify-between'>
+      <div className='md:flex hidden flex-row'>
         <Link to='/' key='home'>
-          <div className='flex flex-row'>
-            <img src={Logo} className='logo my-auto' alt='CardioV' />
-            <span className='my-auto ml-3 text-xl font-bold text-white'> CardioV </span>
-          </div>
+          <img src={Logo} className='logo my-auto' alt='CardioV' />
         </Link>
       </div>
-      <div className='flex items-center text-center mr-8'>
-        <Tooltip title='Account settings'>
-          <IconButton
-            onClick={handleClick}
-            size='small'
-            sx={{ ml: 2 }}
-            aria-controls={openAvatar ? 'account-menu' : undefined}
-            aria-haspopup='true'
-            aria-expanded={openAvatar ? 'true' : undefined}>
-            <Avatar sx={{ width: 42, height: 42 }}>{/* Avatar content */}</Avatar>
-          </IconButton>
-        </Tooltip>
+
+      <div className='flex md:hidden flex-row grow'>
+        <IconButton
+          size='large'
+          aria-label='account of current user'
+          aria-controls='menu-appbar'
+          aria-haspopup='true'
+          onClick={handleOpenNavMenu}
+          color='inherit'>
+          <MenuIcon />
+        </IconButton>
+        <Menu
+          id='menu-appbar'
+          anchorEl={anchorElNav}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          open={Boolean(anchorElNav)}
+          onClose={handleCloseNavMenu}
+          sx={{
+            display: { xs: 'block', md: 'none' },
+          }}>
+          {pages.map(page => (
+            <Link to={page.path} key={page.displayName}>
+              <div className='flex flex-row'>
+                <span className='my-auto text-xl text-center px-2'> {page.displayName} </span>
+              </div>
+            </Link>
+          ))}
+        </Menu>
       </div>
-      <Menu
-        anchorEl={anchorEl}
-        id='account-menu'
-        open={openAvatar}
-        onClose={handleClose}
-        onClick={handleClose}
-        PaperProps={{
-          elevation: 0,
-          sx: {
-            overflow: 'visible',
-            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-            mt: 1.5,
-            '& .MuiAvatar-root': {
-              width: 32,
-              height: 32,
-              ml: -0.5,
-              mr: 1,
-            },
-            '&:before': {
-              content: '""',
-              display: 'block',
-              position: 'absolute',
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
-              zIndex: 0,
-            },
-          },
-        }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}>
-        <MenuItem>
-          <Link to='/profile' key='profile' className='flex flex-row'>
-            <Avatar />
-            <span className='my-auto'>Profile</span>
+      <div className='flex md:hidden flex-row grow'>
+        <Link to='/' key='home'>
+          <img src={Logo} className='logo my-auto' alt='CardioV' />
+        </Link>
+      </div>
+      <div className='md:flex hidden flex-row my-auto'>
+        {pages.map(page => (
+          <Link to={page.path} key={page.displayName}>
+            <div className='flex flex-row'>
+              <span className='my-auto ml-3 text-xl font-bold text-white'> {page.displayName} </span>
+            </div>
           </Link>
-        </MenuItem>
-        <Divider />
-        <MenuItem>
-          <ListItemIcon>
+        ))}
+      </div>
+
+      <div className='flex grow-0'>
+        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+          <Avatar sx={{ width: 42, height: 42 }}>{/* Avatar content */}</Avatar>
+        </IconButton>
+        <Menu
+          sx={{ mt: '45px' }}
+          id='menu-appbar'
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}>
+          <MenuItem>
+            <Link to='/profile' key='profile' className='flex flex-row'>
+              <Avatar />
+              <span className='my-auto'>Profile</span>
+            </Link>
+          </MenuItem>
+          <Divider />
+          <MenuItem>
             <Logout fontSize='small' />
-          </ListItemIcon>
-          <LogoutButton text='Cerrar Sesion' />
-        </MenuItem>
-      </Menu>
+            <LogoutButton text='Cerrar Sesion' />
+          </MenuItem>
+        </Menu>
+      </div>
     </div>
   );
 };
