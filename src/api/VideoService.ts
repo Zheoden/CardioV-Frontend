ec42d3993016223db79bb9e04e326052c44a173b
Zheoden <schifferjulian@gmail.com>
2022-11-02 21:11:36 -0300

    mocked api

--- src/api/VideoService.ts
+++ src/api/VideoService.ts
@@ -2,7 +2,7 @@ import { Profile, VideoRequestBody, VideosDetailsDto, VideosDto } from './Interf
 import client from './VideoClient';
 
 export async function getProfile(): Promise<Profile> {
-  return client
+  /* return client
     .get('/user/me')
     .then(response => {
       return response.data;
@@ -12,55 +12,141 @@ export async function getProfile(): Promise<Profile> {
         return registerProfile();
       }
       throw err;
-    });
+    }); */
+  return Promise.resolve({
+    firstName: 'string',
+    lastName: 'string',
+    birthdate: 'string',
+    avatar: 'string',
+  });
 }
 
 export async function registerProfile(): Promise<Profile> {
-  return client
+  /* return client
     .post('/user/register')
     .then(response => {
       return response.data;
     })
     .catch(err => {
       throw err;
-    });
+    }); */
+  return Promise.resolve({
+    firstName: 'string',
+    lastName: 'string',
+    birthdate: 'string',
+    avatar: 'string',
+  });
 }
 
 export async function updateProfile(body: Profile): Promise<Profile> {
-  return client
+  /* return client
     .patch('/user/me', body)
     .then(response => {
       return response.data;
     })
     .catch(err => {
       throw err;
-    });
+    }); */
+  return Promise.resolve({
+    firstName: 'string',
+    lastName: 'string',
+    birthdate: 'string',
+    avatar: 'string',
+  });
 }
 
 export async function getVideos(filter: string = ''): Promise<VideosDto[]> {
-  return client
+  /* return client
     .get(`/media/me?query=${filter}`)
     .then(response => {
       return response.data;
     })
     .catch(err => {
       throw err;
-    });
+    }); */
+  return Promise.resolve([
+    {
+      id: 'string',
+      title: 'string',
+      description: 'string',
+      thumbnail: 'string',
+      parameters: [
+        { field: 'string', value: 'string' },
+        { field: 'string', value: 'string' },
+        { field: 'string', value: 'string' },
+      ],
+    },
+    {
+      id: 'string',
+      title: 'string',
+      description: 'string',
+      thumbnail: 'string',
+      parameters: [
+        { field: 'string', value: 'string' },
+        { field: 'string', value: 'string' },
+        { field: 'string', value: 'string' },
+      ],
+    },
+    {
+      id: 'string',
+      title: 'string',
+      description: 'string',
+      thumbnail: 'string',
+      parameters: [
+        { field: 'string', value: 'string' },
+        { field: 'string', value: 'string' },
+        { field: 'string', value: 'string' },
+      ],
+    },
+    {
+      id: 'string',
+      title: 'string',
+      description: 'string',
+      thumbnail: 'string',
+      parameters: [
+        { field: 'string', value: 'string' },
+        { field: 'string', value: 'string' },
+        { field: 'string', value: 'string' },
+      ],
+    },
+    {
+      id: 'string',
+      title: 'string',
+      description: 'string',
+      thumbnail: 'string',
+      parameters: [
+        { field: 'string', value: 'string' },
+        { field: 'string', value: 'string' },
+        { field: 'string', value: 'string' },
+      ],
+    },
+  ]);
 }
 
 export async function getVideosById(id: string): Promise<VideosDetailsDto> {
-  return client
+  /*  return client
     .get(`/media/${id}`)
     .then(response => {
       return response.data;
     })
     .catch(err => {
       throw err;
-    });
+    }); */
+  return Promise.resolve({
+    id: 'string',
+    title: 'string',
+    description: 'string',
+    thumbnail: 'string',
+    parameters: [
+      { field: 'string', value: 'string' },
+      { field: 'string', value: 'string' },
+      { field: 'string', value: 'string' },
+    ],
+  });
 }
 
 export async function uploadVideo(body: VideoRequestBody): Promise<void> {
-  const formData = new FormData();
+  /*   const formData = new FormData();
   if (body.currentFile) {
     formData.append('file', body.currentFile);
   }
@@ -73,16 +159,24 @@ export async function uploadVideo(body: VideoRequestBody): Promise<void> {
     })
     .catch(err => {
       throw err;
-    });
+    }); */
+  return Promise.resolve();
 }
 
 export async function deleteVideoById(id: string): Promise<VideosDetailsDto> {
-  return client
+  /*   return client
     .delete(`/media/${id}`)
     .then(response => {
       return response.data;
     })
     .catch(err => {
       throw err;
-    });
+    }); */
+  return Promise.resolve({
+    id: 'string',
+    title: 'string',
+    description: 'string',
+    thumbnail: 'string',
+    parameters: [],
+  });
 }
