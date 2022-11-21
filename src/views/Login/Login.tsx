ec0570e21ce3c70375b27578cc69b04926af245c
giuliposteraro <38227874+giuliposteraro@users.noreply.github.com>
2022-11-21 20:52:58 -0300

    cambios en la vista de perfil (con errores)

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,13 +1,14 @@
 import LoginButton from 'src/components/Session/LoginButton';
 import Logo from '../../assets/Logo-Oscuro.png';
+import loginImage from '../../assets/LoginImage.jpg';
 
 const Login = () => {
-  const loginImage =
-    'https://media.istockphoto.com/vectors/heart-ultrasound-concept-in-flat-style-vector-vector-id1126809074?k=20&m=1126809074&s=612x612&w=0&h=TxjzEj0Bm_yMxQUVDzzhHi1204DeghXxMtZH7wKY2XI=';
+  // const loginImage =
+  // 'https://media.istockphoto.com/id/1126809074/vector/heart-ultrasound-concept-in-flat-style-vector.jpg?s=612x612&w=0&k=20&c=wKRLv0oBehXrohYQ0K_J3a3LWf2JwIG2xb8ELIjggbg=';
 
   return (
     <div className='flex flex-row h-screen w-screen'>
-      <img className='md:flex hidden w-4/6' src={loginImage} />
+      <img className='md:flex hidden w-4/6 h-11/12' src={loginImage} />
       <div className='flex-1 p-16 bg-slate-50 shadow-2xl'>
         <div className='flex flex-col h-full'>
           <div className='flex flex-col my-auto'>
--- src/views/Profile/Profile.tsx
+++ src/views/Profile/Profile.tsx
@@ -5,6 +5,8 @@ import { updateProfile } from 'src/api/VideoService';
 import { useContextState } from 'src/common/ContextState/ContextState';
 import { ActionTypes } from 'src/common/ContextState/Interfaces';
 import Layout from '../Layout/Layout';
+import { Box, Container, Grid, Typography } from '@mui/material';
+import { Avatar, Card, CardActions, CardContent, Divider, CardHeader } from '@mui/material';
 
 const ProfileView = () => {
   const { contextState, setContextState } = useContextState();
@@ -41,41 +43,134 @@ const ProfileView = () => {
 
   return (
     <Layout>
-      <div className='flex flex-col w-full mt-8'>
-        <div className='flex flex-row mx-auto'>
-          <TextField label='Nombre' variant='standard' value={user.firstName} onChange={value => handleUpdate('firstName', value.target.value)} />
-          <div className='ml-8'>
-            <TextField label='Apellido' variant='standard' value={user.lastName} onChange={value => handleUpdate('lastName', value.target.value)} />
+      <div className='flex flex-col w-1/2 mt-8'>
+        <Grid container spacing={3}>
+          <Grid item lg={4} md={6} xs={12}>
+            <Card>
+              <CardContent>
+                <Box
+                  sx={{
+                    alignItems: 'center',
+                    display: 'flex',
+                    flexDirection: 'column',
+                  }}>
+                  <Avatar
+                    src={user.avatar}
+                    sx={{
+                      height: 64,
+                      mb: 2,
+                      width: 64,
+                    }}
+                  />
+                  <Typography color='textPrimary' gutterBottom variant='h5'>
+                    {user.firstName} {user.lastName}
+                  </Typography>
+                </Box>
+              </CardContent>
+              <Divider />
+              <CardActions>
+                <Button color='primary' fullWidth variant='text'>
+                  Subir imagen
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <div className='flex flex-row mx-auto'>
+            <Grid item lg={8} md={6} xs={12}>
+              <Card>
+                <CardHeader title='Datos' />
+                <Divider />
+                <CardContent>
+                  <Grid container spacing={3}>
+                    <Grid item md={6} xs={12}>
+                      <TextField
+                        label='Nombre'
+                        variant='standard'
+                        value={user.firstName}
+                        onChange={value => handleUpdate('firstName', value.target.value)}
+                      />
+                    </Grid>
+                    <Grid item md={6} xs={12}>
+                      <TextField
+                        label='Apellido'
+                        variant='standard'
+                        value={user.lastName}
+                        onChange={value => handleUpdate('lastName', value.target.value)}
+                      />
+                    </Grid>
+                    <Grid item md={6} xs={12}>
+                      <TextField
+                        label='Fecha de Nacimiento'
+                        variant='standard'
+                        type='date'
+                        value={user.birthdate}
+                        sx={{ width: 200 }}
+                        onChange={value => handleUpdate('birthdate', value.target.value)}
+                      />
+                    </Grid>
+                  </Grid>
+                </CardContent>
+                <Divider />
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    p: 2,
+                  }}>
+                  <Button
+                    variant='outlined'
+                    onClick={handleUpdateProfile}
+                    disabled={
+                      contextState.user.firstName === user.firstName &&
+                      contextState.user.lastName === user.lastName &&
+                      contextState.user.birthdate === user.birthdate &&
+                      contextState.user.avatar === user.avatar
+                    }>
+                    Guardar
+                  </Button>
+                </Box>
+              </Card>
+            </Grid>
           </div>
-        </div>
-        <div className='flex flex-row mt-4 mx-auto'>
-          <TextField
-            label='Fecha de Nacimiento'
-            variant='standard'
-            type='date'
-            value={user.birthdate}
-            sx={{ width: 200 }}
-            onChange={value => handleUpdate('birthdate', value.target.value)}
-          />
-          <div className='ml-8'>
-            <TextField label='Avatar' variant='standard' value={user.avatar} onChange={value => handleUpdate('avatar', value.target.value)} />
-          </div>
-        </div>
-        <div className='flex flex-row mt-8 mx-auto'>
-          <Button
-            variant='outlined'
-            onClick={handleUpdateProfile}
-            disabled={
-              contextState.user.firstName === user.firstName &&
-              contextState.user.lastName === user.lastName &&
-              contextState.user.birthdate === user.birthdate &&
-              contextState.user.avatar === user.avatar
-            }>
-            Guardar
-          </Button>
-        </div>
+        </Grid>
       </div>
     </Layout>
+    // <Layout>
+    //   <div className='flex flex-col w-full mt-8'>
+    //     <div className='flex flex-row mx-auto'>
+    //       <TextField label='Nombre' variant='standard' value={user.firstName} onChange={value => handleUpdate('firstName', value.target.value)} />
+    //       <div className='ml-8'>
+    //         <TextField label='Apellido' variant='standard' value={user.lastName} onChange={value => handleUpdate('lastName', value.target.value)} />
+    //       </div>
+    //     </div>
+    //     <div className='flex flex-row mt-4 mx-auto'>
+    //       <TextField
+    //         label='Fecha de Nacimiento'
+    //         variant='standard'
+    //         type='date'
+    //         value={user.birthdate}
+    //         sx={{ width: 200 }}
+    //         onChange={value => handleUpdate('birthdate', value.target.value)}
+    //       />
+    //       <div className='ml-8'>
+    //         <TextField label='Avatar' variant='standard' value={user.avatar} onChange={value => handleUpdate('avatar', value.target.value)} />
+    //       </div>
+    //     </div>
+    //     <div className='flex flex-row mt-8 mx-auto'>
+    //       <Button
+    //         variant='outlined'
+    //         onClick={handleUpdateProfile}
+    //         disabled={
+    //           contextState.user.firstName === user.firstName &&
+    //           contextState.user.lastName === user.lastName &&
+    //           contextState.user.birthdate === user.birthdate &&
+    //           contextState.user.avatar === user.avatar
+    //         }>
+    //         Guardar
+    //       </Button>
+    //     </div>
+    //   </div>
+    // </Layout>
   );
 };
 
