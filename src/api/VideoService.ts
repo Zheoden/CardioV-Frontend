83a2cf665d7db1d155761a779f1eb65bcabcd83d
Zheoden <schifferjulian@gmail.com>
2022-11-26 20:49:41 -0300

    fix service

--- src/api/VideoService.ts
+++ src/api/VideoService.ts
@@ -1,182 +1,89 @@
 import { Profile, VideoRequestBody, VideosDetailsDto, VideosDto } from './Interfaces';
 import client from './VideoClient';
 
-export async function getProfile(): Promise<Profile> {
-  /* return client
+export async function getProfile(profile?: Profile): Promise<Profile> {
+  return client
     .get('/user/me')
     .then(response => {
       return response.data;
-	@@ -12,148 +12,55 @@ export async function getProfile(): Promise<Profile> {
-        return registerProfile();
+    })
+    .catch(err => {
+      if (err.response.data.message === 'USER_NOT_FOUND') {
+        return registerProfile(profile);
       }
       throw err;
-    }); */
-  return Promise.resolve({
-    firstName: 'string',
-    lastName: 'string',
-    birthdate: 'string',
-    avatar: 'string',
-  });
+    });
 }
 
-export async function registerProfile(): Promise<Profile> {
-  /* return client
-    .post('/user/register')
+export async function registerProfile(profile?: Profile): Promise<Profile> {
+  return client
+    .post('/user/register', profile)
     .then(response => {
       return response.data;
     })
     .catch(err => {
       throw err;
-    }); */
-  return Promise.resolve({
-    firstName: 'string',
-    lastName: 'string',
-    birthdate: 'string',
-    avatar: 'string',
-  });
+    });
 }
 
 export async function updateProfile(body: Profile): Promise<Profile> {
-  /* return client
+  return client
     .patch('/user/me', body)
     .then(response => {
       return response.data;
     })
     .catch(err => {
       throw err;
-    }); */
-  return Promise.resolve({
-    firstName: 'string',
-    lastName: 'string',
-    birthdate: 'string',
-    avatar: 'string',
-  });
+    });
 }
 
 export async function getVideos(filter: string = ''): Promise<VideosDto[]> {
-  /* return client
+  return client
     .get(`/media/me?query=${filter}`)
     .then(response => {
       return response.data;
     })
     .catch(err => {
       throw err;
-    }); */
-  return Promise.resolve([
-    {
-      patology: 'string',
-      id: 'string',
-      title: 'string',
-      description: 'string',
-      thumbnail: 'string',
-      parameters: [
-        { field: 'ATRIUM_AREA', value: 'string', unit: 'string' },
-        { field: 'MUSCLE_THICKNESS', value: 'string', unit: 'string' },
-        { field: 'VENTRICLE_AREA', value: 'string', unit: 'string' },
-      ],
-    },
-    {
-      patology: 'string',
-      id: 'string',
-      title: 'string',
-      description: 'string',
-      thumbnail: 'string',
-      parameters: [
-        { field: 'string', value: 'string', unit: 'string' },
-        { field: 'string', value: 'string', unit: 'string' },
-        { field: 'string', value: 'string', unit: 'string' },
-      ],
-    },
-    {
-      patology: 'string',
-      id: 'string',
-      title: 'string',
-      description: 'string',
-      thumbnail: 'string',
-      parameters: [
-        { field: 'string', value: 'string' },
-        { field: 'string', value: 'string' },
-        { field: 'string', value: 'string' },
-      ],
-    },
-    {
-      patology: 'string',
-      id: 'string',
-      title: 'string',
-      description: 'string',
-      thumbnail: 'string',
-      parameters: [
-        { field: 'string', value: 'string', unit: 'string' },
-        { field: 'string', value: 'string', unit: 'string' },
-        { field: 'string', value: 'string', unit: 'string' },
-      ],
-    },
-    {
-      patology: 'string',
-      id: 'string',
-      title: 'string',
-      description: 'string',
-      thumbnail: 'string',
-      parameters: [
-        { field: 'string', value: 'string', unit: 'string' },
-        { field: 'string', value: 'string', unit: 'string' },
-        { field: 'string', value: 'string', unit: 'string' },
-      ],
-    },
-  ]);
+    });
 }
 
 export async function getVideosById(id: string): Promise<VideosDetailsDto> {
-  /*  return client
+  return client
     .get(`/media/${id}`)
     .then(response => {
       return response.data;
     })
     .catch(err => {
       throw err;
-    }); */
-  return Promise.resolve({
-    patology: 'string',
-    id: 'string',
-    title: 'string',
-    description: 'string',
-    thumbnail: 'string',
-    parameters: [
-      { field: 'VENTRICLE_AREA', value: 1, unit: 'string' },
-      { field: 'VENTRICLE_VOLUME', value: 2, unit: 'string' },
-      { field: 'MUSCLE_THICKNESS', value: 3, unit: 'string' },
-      { field: 'ATRIUM_AREA', value: 4, unit: 'string' },
-    ],
-  });
+    });
 }
 
 export async function uploadVideo(body: VideoRequestBody): Promise<void> {
-  /*   const formData = new FormData();
+  const formData = new FormData();
   if (body.currentFile) {
     formData.append('file', body.currentFile);
   }
-	@@ -167,24 +74,16 @@ export async function uploadVideo(body: VideoRequestBody): Promise<void> {
+  formData.append('description', body.description);
+  formData.append('title', body.title);
+  formData.append('patology', body.patology);
+  return client
+    .post(`/media`, formData)
+    .then(response => {
+      return response.data;
     })
     .catch(err => {
       throw err;
-    }); */
-  return Promise.resolve();
+    });
 }
 
 export async function deleteVideoById(id: string): Promise<VideosDetailsDto> {
-  /*   return client
+  return client
     .delete(`/media/${id}`)
     .then(response => {
       return response.data;
     })
     .catch(err => {
       throw err;
-    }); */
-  return Promise.resolve({
-    id: 'string',
-    title: 'string',
-    description: 'string',
-    thumbnail: 'string',
-    parameters: [],
-  });
+    });
 }
