f2466638ad66e31c45bff53823fee4df72ee8916
Zheoden <schifferjulian@gmail.com>
2022-11-26 23:35:14 -0300

    fix video details

--- src/views/VideosDetails/VideosDetails.tsx
+++ src/views/VideosDetails/VideosDetails.tsx
@@ -1,12 +1,9 @@
-import { Card, CardMedia, CardContent, TextField } from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { VideosDetailsDto } from 'src/api/Interfaces';
 import { getVideosById } from 'src/api/VideoService';
 import Layout from '../Layout/Layout';
-
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
-import { Line } from 'react-chartjs-2';
 
 import ListItemText from '@mui/material/ListItemText';
 import Avatar from '@mui/material/Avatar';
@@ -14,28 +11,18 @@ import MonitorHeartIcon from '@mui/icons-material/MonitorHeart';
 import MedicalInformationTwoToneIcon from '@mui/icons-material/MedicalInformationTwoTone';
 import TroubleshootIcon from '@mui/icons-material/Troubleshoot';
 import SquareFootIcon from '@mui/icons-material/SquareFoot';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+import { Chart, CategoryScale, LinearScale, PointElement, LineElement, Tooltip } from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
-export const options = {
-  responsive: true,
-  maintainAspectRatio: false,
-  plugins: {
-    legend: {
-      position: 'top' as const,
-    },
-    title: {
-      display: true,
-      text: 'Distribucion del volumen Ventricular',
-    },
-  },
-};
+Chart.register(CategoryScale, LinearScale, PointElement, LineElement, Tooltip);
 
 const VideosDetails = () => {
   const params = useParams();
   const [videoDetails, setVideoDetails] = useState<VideosDetailsDto>();
   const [videoExtension, setVideoExtension] = useState<string>('');
-  const [atriumArea, setAtriumArea] = useState<number>(0);
+  const [ventriclePerimeter, setventriclePerimeter] = useState<number>(0);
   const [muscleThickness, setMuscleThickness] = useState<number>(0);
   const [ventricleArea, setVentricleArea] = useState<number>(0);
   const [ventricleVolume, setVentricleVolume] = useState<number[]>([]);
@@ -52,13 +39,15 @@ const VideosDetails = () => {
   }, []);
 
   useEffect(() => {
-    const atrium = videoDetails?.parameters.filter(param => param.field === 'ATRIUM_AREA');
+    const ventriclePerimeter = videoDetails?.parameters.filter(param => param.field === 'VENTRICLE_PERIMETER');
     const muscle = videoDetails?.parameters.filter(param => param.field === 'MUSCLE_THICKNESS');
     const ventricleArea = videoDetails?.parameters.filter(param => param.field === 'VENTRICLE_AREA');
     const ventricleVolume = videoDetails?.parameters.filter(param => param.field === 'VENTRICLE_VOLUME');
 
-    if (atrium && atrium.length > 0) {
-      setAtriumArea(atrium.map(atrium => Number(atrium.value)).reduce((a, b) => a + b) / atrium.length);
+    if (ventriclePerimeter && ventriclePerimeter.length > 0) {
+      setventriclePerimeter(
+        ventriclePerimeter.map(ventriclePerimeter => Number(ventriclePerimeter.value)).reduce((a, b) => a + b) / ventriclePerimeter.length,
+      );
     }
 
     if (muscle && muscle.length > 0) {
@@ -75,7 +64,7 @@ const VideosDetails = () => {
   return (
     <Layout>
       <div className='flex flex-col'>
-        <Card className='mx-auto mt-8'>
+        <div className='mx-auto mt-8'>
           {videoDetails &&
             videoExtension &&
             (videoExtension === 'mp4' || videoExtension === 'avi' ? (
@@ -83,84 +72,108 @@ const VideosDetails = () => {
             ) : (
               <img src={videoDetails.thumbnail} className='flex mx-auto source-card' height='140' width='140' />
             ))}
-          <CardContent>
-            <h5 className='flex text-2xl mt-0 mb-2 mx-auto w-max'>Detalles</h5>
-            <div className='flex flex-col font-bold'>
-              <div className='flex flex-col max-w-screen-lg'>
-                {!!atriumArea && (
-                  <div className='flex flex-row mt-5'>
-                    <div className='flex my-auto mr-4'>
-                      <Avatar>
-                        <SquareFootIcon />
-                      </Avatar>
+          <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls='panel1a-content' id='panel1a-header'>
+              <h5 className='flex text-2xl mt-0 mb-2 mx-auto w-max'>Detalles</h5>
+            </AccordionSummary>
+            <AccordionDetails>
+              <div className='flex flex-col font-bold'>
+                <div className='flex flex-col max-w-screen-lg'>
+                  {!!ventriclePerimeter && (
+                    <div className='flex flex-row mt-5'>
+                      <div className='flex my-auto mr-4'>
+                        <Avatar>
+                          <SquareFootIcon />
+                        </Avatar>
+                      </div>
+                      <ListItemText primary='Perimetro Ventricular' secondary={ventriclePerimeter + ' cm3'} />
                     </div>
-                    <ListItemText primary='Area Auricular' secondary={atriumArea + ' cm3'} />
-                  </div>
-                )}
-                {!!muscleThickness && (
-                  <div className='flex flex-row mt-5'>
-                    <div className='flex my-auto mr-4'>
-                      <Avatar>
-                        <MonitorHeartIcon />
-                      </Avatar>
+                  )}
+                  {!!muscleThickness && (
+                    <div className='flex flex-row mt-5'>
+                      <div className='flex my-auto mr-4'>
+                        <Avatar>
+                          <MonitorHeartIcon />
+                        </Avatar>
+                      </div>
+                      <ListItemText primary='Espesor de las paredes' secondary={muscleThickness + ' cm3'} />
                     </div>
-                    <ListItemText primary='Espesor de las paredes' secondary={muscleThickness + ' cm3'} />
-                  </div>
-                )}
-                {!!ventricleArea && (
-                  <div className='flex flex-row mt-5'>
-                    <div className='flex my-auto mr-4'>
-                      <Avatar>
-                        <TroubleshootIcon color='inherit' />
-                      </Avatar>
+                  )}
+                  {!!ventricleArea && (
+                    <div className='flex flex-row mt-5'>
+                      <div className='flex my-auto mr-4'>
+                        <Avatar>
+                          <TroubleshootIcon color='inherit' />
+                        </Avatar>
+                      </div>
+                      <ListItemText primary='Area Ventricular' secondary={ventricleArea + ' cm3'} />
                     </div>
-                    <ListItemText primary='Area Ventricular' secondary={ventricleArea + ' cm3'} />
-                  </div>
-                )}
-                {ventricleVolume.length === 1 && (
-                  <div className='flex flex-row mt-5'>
-                    <div className='flex my-auto mr-4'>
-                      <Avatar>
-                        <MedicalInformationTwoToneIcon />
-                      </Avatar>
+                  )}
+                  {ventricleVolume.length === 1 && (
+                    <div className='flex flex-row mt-5'>
+                      <div className='flex my-auto mr-4'>
+                        <Avatar>
+                          <MedicalInformationTwoToneIcon />
+                        </Avatar>
+                      </div>
+                      <ListItemText primary='Volumen Ventricular' secondary={ventricleVolume[0] + ' cm3'} />
                     </div>
-                    <ListItemText primary='Volumen Ventricular' secondary={ventricleVolume[0] + ' cm3'} />
-                  </div>
-                )}
+                  )}
+                </div>
               </div>
-            </div>
-          </CardContent>
-        </Card>
+            </AccordionDetails>
+          </Accordion>
+        </div>
+
         {ventricleVolume.length > 1 && (
-          <div className='flex flex-col w-6/12 h-60 mx-auto'>
-            <Line
-              options={options}
-              data={{
-                labels: ventricleVolume.map((_, index) => String(index)),
-                datasets: [
-                  {
-                    label: 'Volumen Ventricular',
-                    data: ventricleVolume,
-                    borderColor: 'rgb(255, 99, 132)',
-                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
-                  },
-                ],
-              }}
-            />
+          <div className='flex flex-col w-6/12 mx-auto mt-8'>
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls='panel1a-content' id='panel1a-header'>
+                <Typography>Distribucion del volumen Ventricular</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <div className='flex flex-col mx-auto'>
+                  <Line
+                    options={{
+                      responsive: true,
+                    }}
+                    data={{
+                      labels: ventricleVolume.map((_, index) => String(index)),
+                      datasets: [
+                        {
+                          label: 'Volumen Ventricular',
+                          data: ventricleVolume,
+                          borderColor: 'rgb(255, 99, 132)',
+                          backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                        },
+                      ],
+                    }}
+                  />
+                </div>
+              </AccordionDetails>
+            </Accordion>
           </div>
         )}
-        <div className='flex flex-col mx-auto'>
+        <div className='flex flex-col w-6/12 mx-auto mb-1'>
           {videoDetails?.media.map(media => {
             const mediaParts = media.thumbnail.split('.');
             const mediaExtension = mediaParts[mediaParts.length - 1];
             return (
-              <div className='flex flex-col ml-4' key={media.thumbnail}>
-                <h2>{media.title}</h2>
-                {mediaExtension === 'mp4' || mediaExtension === 'avi' ? (
-                  <video className='source-card-media mx-auto' src={media.thumbnail} controls />
-                ) : (
-                  <img src={media.thumbnail} className='flex mx-auto source-card-media' height='140' width='140' />
-                )}
+              <div className='mt-4' key={media.thumbnail}>
+                <Accordion>
+                  <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls='panel1a-content' id='panel1a-header'>
+                    <Typography>{media.title}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <div className='flex flex-col ml-4'>
+                      {mediaExtension === 'mp4' || mediaExtension === 'avi' ? (
+                        <video className='source-card-media mx-auto' src={media.thumbnail} controls />
+                      ) : (
+                        <img src={media.thumbnail} className='flex mx-auto source-card-media' height='140' width='140' />
+                      )}
+                    </div>
+                  </AccordionDetails>
+                </Accordion>
               </div>
             );
           })}
